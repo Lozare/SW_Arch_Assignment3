bc3d3e22f4ecf1a4b5c4eb0165ef536d13c4cd9b
Lozare <mpm308@msstate.edu>
2017-09-25 17:13:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import sqlite3
 from sqlite3 import Error
 def editQuantityInInventoryDb(dbConnection,item,newQuantity):
     dbCursor = dbConnection.cursor()
-    dbCursor.execute("UPDATE inventory SET numberInStock = '"+str(newQuantity)+
-                     "' WHERE item = '"+item+"';")
+    dbCursor.execute("update inventory set numberInStock = '"+str(newQuantity)+
+                     "' where item = '"+item+"';")
     return
 
 def createDbConnection(dbFile):
@@ -62,7 +62,7 @@ def ShowCategories():
     global mycart
     mycart = ShoppingCart()
     while Ready2Checkout == 0:
-        print "1 - household items\n2 - books\n3 - toys\n4 - small electronics\n5 - clothes\ntotal - show cart total\ncartitems - view items and quantities in cart\nupdate - update item quantities from cart\ncheckout - checkout and pay"
+        print "1 - household items\n2 - books\n3 - toys\n4 - small electronics\n5 - clothes\ntotal - show cart total\ncartitems - view items and quantities in cart\nremove - remove item from cart\ncheckout - checkout and pay\nQuit - Log out"
         choice = raw_input("Enter the number or keyword for your choice: ")
 
         if choice == "1":
@@ -75,20 +75,13 @@ def ShowCategories():
             dbCursor.execute("SELECT price FROM inventory WHERE item='toothpaste' OR item='toilet paper' ORDER BY item DESC")
             print dbCursor.fetchall()
 
-
             choice = raw_input("Enter the name of the item to add it to your cart: ")
             if choice == "toothpaste":
-                dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='toothpaste'")
-                raw_stock = dbCursor.fetchone()
-                print "Stock of this item:", raw_stock[0]
                 choice = raw_input("Enter the quantity desired: ")
                 mycart.add_item('toothpaste', 2.50, float(choice))
                 print mycart.total
 
             if choice == "toilet paper":
-                dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='toilet paper'")
-                raw_stock = dbCursor.fetchone()
-                print "Stock of this item:", raw_stock[0]
                 choice = raw_input("Enter the quantity desired: ")
                 mycart.add_item('toilet paper', 1.99, float(choice))
                 print mycart.total
@@ -108,9 +101,6 @@ def ShowCategories():
 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "SW Design Book":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='SW Design Book'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('SW Design Book', 99.99, float(choice))
                     print mycart.total
@@ -126,9 +116,6 @@ def ShowCategories():
 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "SW Design Book":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='SW Design Book'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('SW Design Book', 99.99, float(choice))
                     print mycart.total
@@ -149,9 +136,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "doll":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='doll'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('doll', 9.99, float(choice))
                     print mycart.total
@@ -167,9 +151,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "Spiderman Figure":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='Spiderman Figure'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('Spiderman Figure', 9.99, float(choice))
                     print mycart.total
@@ -185,9 +166,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "Spiderman Figure":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='Spiderman Figure'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('Spiderman Figure', 9.99, float(choice))
                     print mycart.total
@@ -207,9 +185,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "camera":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='camera'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('camera', 9.99, float(choice))
                     print mycart.total
@@ -225,9 +200,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "mobile phone":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='mobile phone'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('mobile phone', 599.99, float(choice))
                     print mycart.total
@@ -243,9 +215,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "mobile phone":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='mobile phone'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('mobile phone', 599.99, float(choice))
                     print mycart.total
@@ -261,13 +230,10 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "camera":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='camera'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('camera', 9.99, float(choice))
                     print mycart.total
-                    
+                    r
 
                     
         elif choice == "5":
@@ -283,9 +249,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "shirt":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='shirt'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('shirt', 19.99, float(choice))
                     print mycart.total
@@ -301,9 +264,6 @@ def ShowCategories():
                 
                 choice = raw_input("Enter the name of the item to add it to your cart: ")
                 if choice == "shirt":
-                    dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='shirt'")
-                    raw_stock = dbCursor.fetchone()
-                    print "Stock of this item:", raw_stock[0]
                     choice = raw_input("Enter the quantity desired: ")
                     mycart.add_item('shirt', 19.99, float(choice))
                     print mycart.total
@@ -315,10 +275,10 @@ def ShowCategories():
         elif choice == "cartitems":
             print mycart.items
 
-        elif choice == "update":
+        elif choice == "remove":
             print mycart.items
-            choice = raw_input("Enter the name of the item you'd like to change the quantity of in your cart:")
-            choice_quant = raw_input("Enter the quantity you'd like to change it to :")
+            choice = raw_input("Enter the name of the item you'd like to remove from your cart:")
+            choice_quant = raw_input("Enter the quantity you'd like to remove :")
             choice_price = dbCursor.execute("SELECT price FROM inventory WHERE item = '"+choice+"'")
             raw_price = dbCursor.fetchone()
             mycart.remove_item(choice,float(raw_price[0]), choice_quant)
@@ -328,32 +288,84 @@ def ShowCategories():
             print "Here is what we have in your cart:", mycart.items
             print ("Your total to be paid is: $%.2f" % round(mycart.total,2))
             Ready2Checkout = 1
-                    
+            if(mycart.total == 0.00):
+                print("Not Payable Amount")
+                ShowCategories()
+            else:
+                Pay()
+            
+        elif choice == "Quit":
+            print("Logged Off")
+            askUser()
+            
+        else:
+            print("Invalid input")
+            ShowCategories()
+
+
+def askUser():
+    username = raw_input("Enter your Username: ")
+    password = raw_input("Enter your Password: ")
+    checkpass(username, password)
+    return
+
+def checkpass(user, pwd):
+    dbConnection = createDbConnection("assignment3.db")
+    dbCursor = dbConnection.cursor()
+    check = dbCursor.execute("SELECT username FROM account WHERE username = '"+user+"'")
+    check = dbCursor.fetchone()
+
+    if check[0] == user:
+        password = dbCursor.execute("SELECT password FROM account WHERE username = '"+user+"'")
+        password = dbCursor.fetchone()
+        try:
+            if(password[0] == pwd):
+                login(user)
+        except:
+            print("Your username and/or password is incorrect")
+            askUser()
+        else:
+            print("Your username and/or password is incorrect")
+            askUser()
+    else:
+        print("Your username and/or password is incorrect")
+        askUser()
+
+       
+
+        
+
+def login(user):
+    print("Welcome " + user)
+    print("You have successfully login in!")
+    ShowCategories()
+
+
+def createPayment():
+    card = raw_input("Enter 16 digit card number: ")
+    if len(str(card)) != 16:
+        print("Not valid card number")
+        createPayment()
+    else:
+        return card
+
+def createAddress():
+    address = raw_input("Enter Address: ")
+    return address
+
+def Pay():
+    card = createPayment()
+    address = createAddress()
+    
+    
+    
+    
     
         
 def main():
     dbConnection = createDbConnection("assignment3.db")
     inventory = buildInventory(dbConnection)
-    ShowCategories()
-    dbCursor = dbConnection.cursor()
-    key_array = []
-    quant_array = []
-    
-    for key in mycart.items:
-        key_array.append(key)
-    for each in mycart.items:
-        quant_array.append(mycart.items[each])
-
-    i=0
-    for each in key_array:
-        dbCursor.execute("SELECT numberInStock FROM inventory WHERE item='"+each+"'")
-        raw_stock = dbCursor.fetchone()
-        newquant = raw_stock[0] - quant_array[i]
-        editQuantityInInventoryDb(dbConnection,each,newquant)
-        i+=1
-
-    
-        
+    askUser()
     
     
     
